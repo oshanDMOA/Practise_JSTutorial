877b4e8b56c7ed60253d19ba8ad79c8b0b5d3075
oshanDMOA <capitalassignments67@gmail.com>
2024-07-17 16:24:27 +0530

    pushing the folder

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+var name = "some-name"
+console.log("some message from javascript file")
+
+
+
+var TotalPrice = 19
+var ShppingCost;
+
+if (TotalPrice > 20) {
+    ShppingCost = 0 
+} else {
+    ShppingCost = 5 
+}
+if (TotalPrice<=20) {
+    ShppingCost = 5
+} else {
+    ShppingCost = 0
+}
